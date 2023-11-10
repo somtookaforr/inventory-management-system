36a76883cae6b5002cc4fbcc3a620e4d805ab5e7
Tycoone <rictoye@gmail.com>
2023-11-10 09:55:58 +0100

    I updated the navigation bar dropdown menu

--- inc/navigation.php
+++ inc/navigation.php
@@ -1,19 +1,30 @@
-	<!-- Navigation -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Your head content here -->
+</head>
+<body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="<?php echo ROOT_URL; ?>">Inventory System</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-			<!-- <li class="nav-item">
-				<form class="form-inline" action="/action_page.php">
-					<input class="form-control col-md-8 mr-sm-2" type="text" placeholder="Search">
-					<button class="btn btn-success" type="submit">Search</button>
-				</form>
-			</li> -->
-			<li class="nav-item">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarDropdown" aria-controls="navbarDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="<?php echo ROOT_URL; ?>">Inventory System</a>
+            <div class="collapse navbar-collapse" id="navbarDropdown">
+                <ul class="navbar-nav ml-auto  d-block d-lg-none d-xl-none d-xxl-none">
+                    <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                        <a class="nav-link active" id="v-pills-index-tab" data-toggle="pill" href="#v-pills-index" role="tab" aria-controls="v-pills-index" aria-selected="true">Index</a>
+                        <a class="nav-link" id="v-pills-item-tab" data-toggle="pill" href="#v-pills-item" role="tab" aria-controls="v-pills-item" aria-selected="false">Item</a>
+                        <a class="nav-link" id="v-pills-purchase-tab" data-toggle="pill" href="#v-pills-purchase" role="tab" aria-controls="v-pills-purchase" aria-selected="false">Purchase</a>
+                        <a class="nav-link" id="v-pills-vendor-tab" data-toggle="pill" href="#v-pills-vendor" role="tab" aria-controls="v-pills-vendor" aria-selected="false">Vendor</a>
+                        <a class="nav-link" id="v-pills-sale-tab" data-toggle="pill" href="#v-pills-sale" role="tab" aria-controls="v-pills-sale" aria-selected="false">Sale</a>
+                        <a class="nav-link" id="v-pills-customer-tab" data-toggle="pill" href="#v-pills-customer" role="tab" aria-controls="v-pills-customer" aria-selected="false">Customer</a>
+                        <a class="nav-link" id="v-pills-search-tab" data-toggle="pill" href="#v-pills-search" role="tab" aria-controls="v-pills-search" aria-selected="false">Search</a>
+                        <a class="nav-link" id="v-pills-reports-tab" data-toggle="pill" href="#v-pills-reports" role="tab" aria-controls="v-pills-reports" aria-selected="false">Reports</a>
+                    </div>
+                </ul>
+
+                <li class="nav-item">
 				<span class="nav-link">Welcome <?php echo $_SESSION['fullName']; ?></span>
             </li>
 			<li class="nav-item">
@@ -22,7 +33,13 @@
 			<li class="nav-item">
 				<a class="nav-link" href="model/login/logout.php">Log Out</a>
             </li>
-          </ul>
+            </div>
         </div>
-      </div>
     </nav>
+
+    <!-- The rest of your HTML content goes here -->
+
+    <!-- Include any necessary JavaScript and CSS links -->
+
+</body>
+</html>
